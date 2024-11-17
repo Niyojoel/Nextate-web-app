d4f4945623a49b45b66238ea7ecb987166842d56
Niyojoel <113913352+Niyojoel@users.noreply.github.com>
2024-11-17 12:02:26 +0100

    updated to remote API uri

--- Frontend/src/lib/apiRequest.js
+++ Frontend/src/lib/apiRequest.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const apiRequest = axios.create({
-  baseURL: "http://localhost:5500/api",
+  baseURL: "https://nextate-api.onrender.com",
   withCredentials: true,
 });
 
