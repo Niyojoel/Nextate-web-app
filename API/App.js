bf524f831e98a5e65b6c464a3599a367e92a2985
Niyojoel <113913352+Niyojoel@users.noreply.github.com>
2024-11-17 12:18:52 +0100

    Update App.js

--- API/App.js
+++ API/App.js
@@ -16,7 +16,7 @@ const app = express();
 app.use(express.static(`${fs.__dirname}/public`));
 app.use(express.json());
 app.use(cookieParser())
-app.use(cors({origin: process.env.CLIENT_URL, credentials: true}))
+app.use(cors({origin: https://nextate-frontend.onrender.com, credentials: true}))
 
 const homePage = fs.readFileSync("./public/index.html", 'utf-8')
 
